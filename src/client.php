ae4a691e66845356b34f9b37a6b4ca106660cd68
Pierre <pierreracco15@gmail.com>
2017-05-10 14:51:06 +0200

    ajout exception

--- src/client.php
+++ src/client.php
@@ -1,17 +1,20 @@
 <html>
 	<body>
 		<h1>Client Guzzle : book store service</h1>
-		<h2>Etat du stock : </h2>
+		<h2>Etat du stock :</h2>
 		<?php
 			require __DIR__ . '/../vendor/autoload.php';
 
 			//todo : appeler /books de shoppingService
-			$clientShoppingService = new GuzzleHttp\Client(['base_uri' => 'http://localhost:5000/']);
-
-			$responseShoppingService = $clientShoppingService->request('GET', 'books');
-
-			$body = $responseShoppingService->getBody();
-			echo $body;
+			
+				$clientShoppingService = new GuzzleHttp\Client(['base_uri' => 'http://localhost:5000/']);
+			try {
+				$responseShoppingService = $clientShoppingService->request('GET', 'books');
+				$body = $responseShoppingService->getBody();
+				echo $body;
+			} catch (Exception $e) {
+				echo "<p> Service indisponible.</p>";
+			}
 		?>
 	</body>
 </html>
